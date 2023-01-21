d6097c48c7002fcc4a278c3e6e185b44a11f9bbe
Bob Coleman <rjcoleman02@gmail.com>
2023-01-21 10:39:02 -0500

    Updated ToDo App for UX and Animations

---
+++ ToDoList/Views/NoItemsView.swift
@@ -0,0 +1,20 @@
+//
+//  NoItemsView.swift
+//  ToDoList
+//
+//  Created by Bob Coleman on 1/21/23.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoItemsView()
+    }
+}
